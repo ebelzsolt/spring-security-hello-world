d13dcd6a747ae37b8511a4c5ae440a58c1f52a36
Zsolt Ébel <zsolt_ebel@epam.com>
2017-11-05 23:17:58 +0100

    ViewResolver typo fixed

--- src/main/java/com/example/security/config/AppConfig.java
+++ src/main/java/com/example/security/config/AppConfig.java
@@ -19,10 +19,10 @@ public class AppConfig {
 		
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		
-		viewResolver.setPrefix("/WEB-INF/view");
+		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
 		
-		return viewResolver();
+		return viewResolver;
 	}
 
 }
