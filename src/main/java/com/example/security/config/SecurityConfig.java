f4a267f9021597a8c5369565746d4b4feb9384ee
Zsolt Ébel <zsolt_ebel@epam.com>
2017-11-05 23:38:21 +0100

    base security

---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// add our users for in memory authentication
+		
+		auth
+			.inMemoryAuthentication()
+			.withUser("admin")
+			.password("admin")
+			.roles("ADMIN");
+		
+	}
+
+}
